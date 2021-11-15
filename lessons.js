fd9fd273857463d829cc4b9628668fa12e1254a8
RayyanDakhni <dakhnirayyan@gmail.com>
2021-11-15 16:41:52 +0500

    Adding lessons.js file to the project

---
+++ lessons.js
@@ -0,0 +1,98 @@
+const lessons = [
+    {
+        id: 1001,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1002,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1003,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1004,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1005,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1006,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1007,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1008,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1009,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1010,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1011,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    },
+    {
+        id: 1012,
+        subject: 'Mathematics',
+        location: 'Lab-1-10',
+        price: '$20.00',
+        spaces: 5,
+        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+    }
+];
