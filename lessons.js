0a75bba73c7bdd5efb90c18ba6416234f4214d4b
RayyanDakhni <dakhnirayyan@gmail.com>
2021-11-15 17:00:26 +0500

    Added relevant data to lessons js file

--- lessons.js
+++ lessons.js
@@ -1,98 +1,98 @@
 const lessons = [
     {
         id: 1001,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'English',
+        location: 'Lab-1-01',
+        price: '$20.50',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://image.freepik.com/free-vector/online-language-school-courses-flat-illustration-woman-watching-lesson-website-learn-english-foreign-language-online-training-e-learning-communication-foreigners-via-internet_204997-165.jpg'
     },
     {
         id: 1002,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'Maths',
+        location: 'Lab-1-03',
+        price: '$18.00',
         spaces: 5,
         image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
     },
     {
         id: 1003,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'History',
+        location: 'Lab-1-05',
+        price: '$14.00',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://img.freepik.com/free-vector/history-concept-illustration-flat-style_277904-9082.jpg?size=626&ext=jpg'
     },
     {
         id: 1004,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'Geography',
+        location: 'Lab-1-07',
+        price: '$15.00',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://image.freepik.com/free-vector/geography-class-concept-illustration_277904-10044.jpg'
     },
     {
         id: 1005,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
+        subject: 'Urdu',
+        location: 'Lab-1-09',
         price: '$20.00',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://cdn.dribbble.com/users/1600851/screenshots/7222587/media/e6a4bddd6294e48c52a97adf2815dbeb.jpg?compress=1&resize=400x300'
     },
     {
         id: 1006,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'Computer Science',
+        location: 'Lab-2-01',
+        price: '$17.50',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://leverageedu.com/blog/wp-content/uploads/2019/12/BSc-Computer-Science-Subjects.jpg'
     },
     {
         id: 1007,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'Physics',
+        location: 'Lab-2-03',
+        price: '$22.50',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://media.sciencephoto.com/image/f0197362/800wm'
     },
     {
         id: 1008,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'Chemistry',
+        location: 'Lab-2-05',
+        price: '$22.50',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://media.istockphoto.com/vectors/chemical-explosion-and-flying-hearts-love-symbol-vector-id1129321196?k=20&m=1129321196&s=612x612&w=0&h=5A2nYK6sJKK79d53GZJxUIKcGMyNKL8JbjmpyerfOFs='
     },
     {
         id: 1009,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'Add Maths',
+        location: 'Lab-2-07',
+        price: '$25.00',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://www.mindstretcher.com/wp-content/uploads/2020/01/website-program-images_SECONDARY-additional-maths.jpg'
     },
     {
         id: 1010,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
+        subject: 'Islamiat',
+        location: 'Lab-2-09',
         price: '$20.00',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://thumbs.dreamstime.com/b/paper-mosque-islamic-festivals-creative-cutout-yellow-cloudy-background-muslim-community-festival-celebration-76363426.jpg'
     },
     {
         id: 1011,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'Arabic',
+        location: 'Lab-3-01',
+        price: '$19.00',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://static.demilked.com/wp-content/uploads/2017/02/illustrated-arabic-words-meaning-mahmoud-el-sayed-10.jpg'
     },
     {
         id: 1012,
-        subject: 'Mathematics',
-        location: 'Lab-1-10',
-        price: '$20.00',
+        subject: 'English Literature',
+        location: 'Lab-3-03',
+        price: '$21.50',
         spaces: 5,
-        image: 'https://img.freepik.com/free-vector/calculator-concept-illustration_114360-1239.jpg?size=338&ext=jpg'
+        image: 'https://cdn.dribbble.com/users/1626229/screenshots/5300417/dont_stop_reading_4x.jpg?compress=1&resize=400x300'
     }
 ];
